92ecea944728cdb8911b324f404692e2ae39f37a
Marvin Clerge <clergemarvin@gmail.com>
2018-06-10 16:08:32 -0400

    project main structure and title fading

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-document.addEventListener('DOMContentLaoded', ()=> {
-  
+document.addEventListener('DOMContentLoaded', ()=> {
+  const titles = [
+    'Full Stack Engineer',
+    'Software Engineer',
+    'Frontend Engineer',
+    'Backend Engineer',
+    'Code Wizard'
+  ]
+
+  let counter = 0
+
+  setInterval(function(){    
+    let title = document.querySelector('#title')
+    
+    if (counter >= titles.length - 1){
+      counter = 0
+    } else {
+      counter++
+    }
+    
+    title.innerHTML = titles[counter]
+  },3000)
 })
