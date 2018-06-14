533aee932881934ea245632dfab76e9f3ab47624
Marvin Clerge <clergemarvin@gmail.com>
2018-06-14 19:59:36 -0400

    video playing functionality

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-  const titles = [
-    'Full Stack Engineer',
-    'Software Engineer',
-    'Frontend Engineer',
-    'Backend Engineer',
-    'Code Wizard'
-  ]
+class App {
+  static init(){
+    this.nameChange()
+    this.playVideo()
+    this.modalExitListener()
+  }
 
-  let counter = 0
 
-  setInterval(function(){    
-    let title = document.querySelector('#title')
+  static nameChange(){
+    const titles = [
+      'Full Stack Engineer', 'Software Engineer', 
+      'Frontend Engineer', 'Backend Engineer', 'Code Wizard'
+    ]
+
+    let counter = 0
+
+    setInterval(function(){
+      counter >= titles.length -1  ? counter = 0 : counter++
+      document.querySelector('#title').innerHTML = titles[counter]
+    },3000)
+  }
+
+
+  static playVideo(){
+    document.querySelectorAll('.overlay').forEach(function(item){
+      item.addEventListener('click', function(event){
+        document.querySelector('iframe')
+          .src = `https://www.youtube.com/embed/${item.dataset.mediaId}`
+        
+        document.querySelector('.modal-content')
+          .parentNode.style.display = 'inline'
+      })
+    })
+  }
+
+  static modalExitListener(){
+    const modal = document.querySelector('.modal')
     
-    if (counter >= titles.length - 1){
-      counter = 0
-    } else {
-      counter++
-    }
     
-    title.innerHTML = titles[counter]
-  },3000)
+    modal.addEventListener('click', function(event){
+      if(event.target.className === 'modal'){
+        modal.style.display = "none"
+        document.querySelector('iframe').src = ""
+      }
+    })
+  }
+
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  App.init()
+  
 })
